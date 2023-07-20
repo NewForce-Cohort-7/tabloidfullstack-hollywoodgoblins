cc388d0e7be8a3c06e4c084e1a1209e561adeca0
JamesB0907 <virverum22@gmail.com>
2023-07-19 22:29:22 -0400

    I've added Categories to the Navbar. Clicking the Navbar item allows the user to see all Categories

---
+++ TabloidFullStack/TabloidFullStack/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using TabloidFullStack.Models;
+
+namespace TabloidFullStack.Repositories
+{
+    public interface ICategoryRepository
+    {
+            void DeleteCategory(int categoryId);
+            List<Category> GetAllCategories();
+            Category GetCategoryById(int id);
+            void UpdateCategory(Category category);
+            void AddCategory(Category category);
+    }
+}
